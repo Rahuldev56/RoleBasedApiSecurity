28613a332bc8574e9e6be979ad0b67f655e5ad59
Rahul Dev <100990357+Rahuldev56@users.noreply.github.com>
2025-05-21 23:19:31 +0530

    Update User.java

--- src/main/java/com/example/SecurityP02/entity/User.java
+++ src/main/java/com/example/SecurityP02/entity/User.java
@@ -9,7 +9,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @OneToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name="user_roles",joinColumns = @JoinColumn(name="user_id"),
             inverseJoinColumns =@JoinColumn(name="role_id"))
     private Set<Role> roles;
